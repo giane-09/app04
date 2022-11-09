82a1f08d6f653c03785836aa877fc8e06964235c
giane-09 <gianelaamv@gmail.com>
2022-11-09 11:11:33 -0500

    trabajo_final

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:app04/pages/alert_page.dart';
 import 'package:app04/pages/avatar_page.dart';
 import 'package:app04/pages/card_page.dart';
+import 'package:app04/pages/input_page.dart';
+import 'package:app04/pages/trabajo_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -66,6 +68,14 @@ class HomePage extends StatelessWidget {
               ItemComponentWidget(
                 title: "Card",
                 toPage: CardPage(),
+              ),
+              ItemComponentWidget(
+                title: "Tarea",
+                toPage: TareaPage(),
+              ),
+              ItemComponentWidget(
+                title: "Imputs",
+                toPage: InputPage(),
               ),
             ],
           ),
---
+++ lib/pages/input_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatelessWidget {
+  const InputPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text("Input page"),
+      ),
+      body: ,
+    );
+  }
+}
